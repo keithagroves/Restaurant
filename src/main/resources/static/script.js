d5ba44a7a222697de52dcca61f02e03b863cf89d
Michael Back <zot@Michaels-MacBook-Air.local>
2020-12-19 11:04:20 +0800

    Changed Host

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,6 +1,7 @@
+const host = "https://lit-river-05482.herokuapp.com";
 async function fetchSpecial () {
 	var request = {method: "get", headers: {'Content-Type': 'application/json'}};
-	var response = await fetch("http://localhost:8080/special", request);
+	var response = await fetch(host + "/special", request);
 	response.text().then (function(res) {
 		console.log(res);
 		alert(res);
@@ -8,7 +9,7 @@ async function fetchSpecial () {
 }
 async function fetchDrink () {
 	var request = {method: "get", headers: {'Content-Type': 'application/json'}};
-	var response = await fetch("http://localhost:8080/drink", request);
+	var response = await fetch(host + "/drink", request);
 	response.text().then (function(res) {
 		console.log(res);
 		var element = document.getElementById("order");
